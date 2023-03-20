5fe9356efb85614bcc18f61f650d905a5f6828fd
simonab101 <simo.buscicchio@gmail.com>
2023-03-20 13:50:22 +0100

    esercizio-svolto

---
+++ scrypt.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+function passwordGenerator() {
+  let name = prompt("Qual è il tuo nome?");
+  let surname = prompt("Qual è il tuo cognome?");
+  let color = prompt("Qual è il tuo colore preferito?");
+  
+  document.getElementById("password-container").style.display = "block";
+  document.getElementById("output").innerHTML = name + surname + color + 21;
+}
